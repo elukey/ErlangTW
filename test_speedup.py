94e777cc399af13cdef916f229ba8ad10f3db83a
Luca Toscano <aa@aa.ot>
2012-05-05 13:05:17 +0200

    - added a python script to automatize simulation's runs

---
+++ test_speedup.py
@@ -0,0 +1,31 @@
+import subprocess
+
+def create_config_file(filepath, content):
+	filefd = open(filepath, 'w')
+	filefd.write(content)
+	filefd.close()
+
+def get_config_file_content(density, lps, max_ts, entities):
+	return "lps="+str(lps)+"\ndensity="+str(density)+"\nentities="+str(entities)+"\nmax_ts="+str(max_ts) 
+
+if __name__ == "__main__":
+	print "\nTesting speed up\n"
+	erlangtw_dir = "/home/luke/Desktop/ErlangTW"
+	tests_dir = erlangtw_dir + "/tests"
+	
+	exp_runs = 100
+	lps_list = [1,2,4,10]
+	density = 0.5
+	entities = 1000
+	max_ts = 1000
+	
+	for lps in lps_list:
+		for run in range(1, exp_runs):
+			print "\nRun " + str(run) + " of " + str(exp_runs) + "\n"  
+			config_file_content = get_config_file_content(density, lps, max_ts, entities)
+			config_file_path = tests_dir + "/" + "config_speedup_lp"+str(lps)+"_run"+str(run)
+			create_config_file(config_file_path, config_file_content)
+			output_file_path = tests_dir + "/" + "output_speedup_lp"+str(lps)+"_run"+str(run)
+			subprocess.call(["./starter",config_file_path, output_file_path])
+	
+	print "\nExperiment completed!\n"
